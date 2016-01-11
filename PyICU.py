55485084c403898dac7c6145ba049c5c7f2909ae
Jakub Wilk <jwilk@jwilk.net>
2016-01-11 10:12:01 +0100

    remove spurious quote from the deprecation warning

--- PyICU.py
+++ PyICU.py
@@ -23,7 +23,7 @@
 
 import warnings as _warnings
 
-_warnings.warn("Module 'PyICU' is deprecated, import 'icu' instead'",
+_warnings.warn("Module 'PyICU' is deprecated, import 'icu' instead",
                category=DeprecationWarning, stacklevel=2)
 
 from icu import *
