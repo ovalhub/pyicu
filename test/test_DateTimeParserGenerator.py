db70fb6b3f4ac5f414e2f5880ec7c5d7afbf7dc6
ovalhub <github@ovaltofu.org>
2016-05-10 09:46:47 -0700

    added timezones so that tests pass from everywhere

--- test/test_DateTimeParserGenerator.py
+++ test/test_DateTimeParserGenerator.py
@@ -32,8 +32,10 @@ class TestDateTimePatternGenerator(TestCase):
 
     def setUp(self):
         cal = Calendar.createInstance()
+        cal.setTimeZone(TimeZone.getGMT())
         cal.set(2016, 4, 10, 0, 30, 0)  # month value is 0-based
         self.date = cal.getTime()
+        self.tz = TimeZone.createTimeZone('America/Los_Angeles')
 
     def testAddPattern(self):
         """Test overwriting a pattern with the same skeleton."""
@@ -41,12 +43,14 @@ class TestDateTimePatternGenerator(TestCase):
         dtpg = DateTimePatternGenerator.createInstance(locale)
 
         sdf = SimpleDateFormat(dtpg.getBestPattern('MMMMddHmm'), locale)
-        self.assertEqual(sdf.format(self.date), u'10 mai à 00:30')
+        sdf.setTimeZone(self.tz)
+        self.assertEqual(sdf.format(self.date), u'09 mai à 17:30')
         self.assertEqual(sdf.toPattern(), u"dd MMMM 'à' HH:mm")
 
         dtpg.addPattern("dd'. von' MMMM", True)
         sdf.applyPattern(dtpg.getBestPattern('MMMMddHmm'))
-        self.assertEqual(sdf.format(self.date), u'10. von mai à 00:30')
+        sdf.setTimeZone(self.tz)
+        self.assertEqual(sdf.format(self.date), u'09. von mai à 17:30')
         self.assertEqual(sdf.toPattern(), u"dd'. von' MMMM 'à' HH:mm")
 
     def testGetBestPattern(self):
@@ -56,12 +60,12 @@ class TestDateTimePatternGenerator(TestCase):
                      'MMMMd',  # full name of month + day of the month, i.e., October 25
                      'hhmm',   # 12-hour-cycle format, i.e., 1:32 PM
                      'jjmm')   # preferred hour format for the given locale, i.e., 24-hour-cycle format for fr_FR
-        locales = ((Locale('en_US'), (u'2nd quarter 2016', u'May 2016', u'May 10',
-                                      u'12:30 AM', u'12:30 AM')),
-                   (Locale('fr_FR'), (u'2e trimestre 2016', u'mai 2016', u'10 mai',
-                                      u'12:30 AM', u'00:30')),
-                   (Locale('zh_CN'), (u'2016年第2季度', u'2016年5月', u'5月10日',
-                                      u'上午12:30', u'上午12:30')),)
+        locales = ((Locale('en_US'), (u'2nd quarter 2016', u'May 2016', u'May 9',
+                                      u'5:30 PM', u'5:30 PM')),
+                   (Locale('fr_FR'), (u'2e trimestre 2016', u'mai 2016', u'9 mai',
+                                      u'5:30 PM', u'17:30')),
+                   (Locale('zh_CN'), (u'2016年第2季度', u'2016年5月', u'5月9日',
+                                      u'下午5:30', u'下午5:30')),)
         for locale, locale_data in locales:
             dtpg = DateTimePatternGenerator.createInstance(locale)
             for index, skeleton in enumerate(skeletons):
@@ -72,18 +76,18 @@ class TestDateTimePatternGenerator(TestCase):
     def testReplaceFieldType(self):
         """Test replacing an element of a pattern."""
         locale = Locale.getFrance()
-        tz = TimeZone.createTimeZone('Europe/Paris')
         dtpg = DateTimePatternGenerator.createInstance(locale)
         sdf = SimpleDateFormat('EEEE d MMMM y HH:mm:ss zzzz', locale)
-        sdf.setTimeZone(tz)
+        sdf.setTimeZone(self.tz)
         pattern = sdf.toPattern()
         self.assertEqual(pattern, u'EEEE d MMMM y HH:mm:ss zzzz')
-        self.assertEqual(sdf.format(self.date), u'lundi 9 mai 2016 23:30:00 heure d’été d’Europe centrale')
+        self.assertEqual(sdf.format(self.date), u'lundi 9 mai 2016 17:30:00 heure d’été du Pacifique')
 
         newPattern = dtpg.replaceFieldTypes(pattern, 'vvvv')
         sdf.applyPattern(newPattern)
+        sdf.setTimeZone(self.tz)
         self.assertEqual(sdf.toPattern(), u'EEEE d MMMM y HH:mm:ss vvvv')
-        self.assertEqual(sdf.format(self.date), u'lundi 9 mai 2016 23:30:00 heure d’Europe centrale')
+        self.assertEqual(sdf.format(self.date), u'lundi 9 mai 2016 17:30:00 heure du Pacifique nord-américain')
 
 
 if __name__ == '__main__':
