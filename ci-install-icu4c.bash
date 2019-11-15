5ec0a9b3df5816e6597d3688aa3ace6b6161b13a
Seth Morton <seth.m.morton@gmail.com>
2019-11-14 18:57:36 -0800

    Set up caching
    Caching is done for:
     - pyenv collateral on macos
     - The icu4c installed collateral for all os's
    
    Based on my research, the cache is unique for each OS, Python version,
    and set of environment variables. Since the ICU version is defined by
    the environment variable, the different installations of ICU versions
    should not clash with each other if we install them into the same
    directory name.

--- ci-install-icu4c.bash
+++ ci-install-icu4c.bash
@@ -3,18 +3,38 @@
 # Fail on first non-successful command, and print out what it is doing.
 set -ex
 
+INSTALL_DIR="$HOME/icu4c"
+BUILD_DIR="$HOME/build"
+
 # Require an argument to pass to runConfigureICU
 platform="${1?}"
+version="${ICU_VERSION?}"
+cache_tag="${INSTALL_DIR}/icu4c-${TRAVIS_OS_NAME?}-${version}.done"
 
 # Installs icu4c on linux from source
-# Requires that the environment variable ICU_VERSION is set.
-ICU_URL="https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-src.tgz"
+ICU_URL="https://github.com/unicode-org/icu/releases/download/release-${version/./-}/icu4c-${version/./_}-src.tgz"
 
-# install ICU4C from source
-mkdir -p "/tmp/icu4c"
-wget -nv -O "/tmp/icu4c/icu4c.tgz" "${ICU_URL}"
-tar -xzf "/tmp/icu4c/icu4c.tgz" -C "/tmp/icu4c"
-cd "/tmp/icu4c/icu/source"
-./runConfigureICU "${platform}" --prefix="/tmp/icu4c"
+# If the INSTALL_DIR already exists, then it was cached and we do not need to rebuild.
+if [ -f "${cache_tag}" ]
+then
+    echo "Found cached icu4c in ${INSTALL_DIR}, skipping build!"
+    exit 0
+fi
+
+# Fetch
+mkdir -p "${BUILD_DIR}"
+wget -nv -O "${BUILD_DIR}/icu4c.tgz" "${ICU_URL}"
+
+# Extract
+tar -xzf "${BUILD_DIR}/icu4c.tgz" -C "${BUILD_DIR}"
+
+# Configure/build
+cd "${BUILD_DIR}/icu/source"
+./runConfigureICU "${platform}" --prefix="${INSTALL_DIR}"
 make
+
+# Install
 make install
+
+# Add a tag to the installation directory to validate the cache
+touch "${cache_tag}"
