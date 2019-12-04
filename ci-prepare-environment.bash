c9d9aec3affad607d4af33f7d67c8e22fa5f6acd
Seth Morton <seth.m.morton@gmail.com>
2019-12-04 15:54:22 -0800

    Expand Travis-CI scope (#118)
    * Travis-CI now recommends to omit sudo altogether
    
    https://blog.travis-ci.com/2018-11-19-required-linux-infrastructure-migration
    
    * Place ICU versions in a matrix
    
    This is likely WAY too many ICU versions to actually test against,
    but it is exhausitve as far as I can tell.
    
    * Test against all currently support Python versions
    
    * Move before_install into the script ci-install-icu4c.bash
    
    It is expected that the installtion instructions for windows and
    will be different from *nix, so to keep the config simple the
    installation instructions have been moved to a separate script.
    
    Use ./runConfigureICU instead of ./configure
    
    * Update build matrix to limit the total number of jobs
    
    Previously, every ICU would be tested against every Python. That's just
    too many jobs.
    
    Now, the latest ICU is always tested, as well as one older version.
    This is done against all Python versions.
    
    Additionally, for the latest Python version all ICU versions are tested.
    
    * Add support for macos
    
    The macos job is at the top because it takes a long time to boot and to
    queue.
    
    Also needed was:
     - Add icu4c bin location to the PATH for icu-config
     - Add DYLD_LIBRARY_PATH... it's like LD_LIBRARY_PATH, but for macos.
    
    * Drop unneeded six installation
    
    * Set up caching
    
    Caching is done for:
     - pyenv collateral on macos
     - The icu4c installed collateral for all os's
    
    Based on my research, the cache is unique for each OS, Python version,
    and set of environment variables. Since the ICU version is defined by
    the environment variable, the different installations of ICU versions
    should not clash with each other if we install them into the same
    directory name.
    
    * List icule as needed library for ICU < 58
    
    * Add configuration information before PyICU build
    
    ICU introspection commands are now invoked so we can be sure
    that we are building against what we think we are building
    
    * Omit ICU version 52.2 from CI
    
    The --cxxflags output from icu-config for this version is broken
    (it gives an argument incompatible with gcc).
    
    * Refactor .travis-ci.yml and build script
    
    Both files have been refactored to make it easier to extend, which needs
    to happen to support windows.
    
    The build script has been broken into functions, and the installation
    of pyenv and the display of ICU configuration has been moved
    to this script to make the .travis-ci.yml cleaner.
    
    The .travis-ci.yml has removed everything that is now in the build
    script, and has also gotten rid of the jobs matrix and now just defines
    explictly all jobs to execute.
    
    * Add potential support for Windows
    
    This adds commented out jobs for Windows. In the bash script, it adds
    building python on windows, fetching and extracting the source code on
    windows, and a broken attempt at building the binaries on windows.
    
    Currently, the build executes for 13 minutes then fails at the end.
    Should someone want to get this working, most of the infrastructure is
    there, they just need to mess with the code that actually builds, and
    then potentially the PATH when actually executing pip and running the
    tests.

---
+++ ci-prepare-environment.bash
@@ -0,0 +1,146 @@
+#! /bin/bash
+
+# Fail on first non-successful command
+set -e
+# set -x  # turn on for debugging
+
+#######
+# Tools
+
+# Grab the ICU source from the web then extract.
+# Parameters:
+#  version: ICU version to download
+#  location: Directory to place the source and to extract the contents.
+function fetch_icu {
+    echo "Downloading and extracting ICU source..."
+    local version="${1?}"
+    local location="${2?}"
+
+    # Download the source as zip on Windows and tgz otherwise
+    local icu_url="https://github.com/unicode-org/icu/releases/download/release-${version/./-}/icu4c-${version/./_}-src.tgz"
+    if [ "${TRAVIS_OS_NAME}" = windows ]; then icu_url="${icu_url%.tgz}.zip"; fi
+    local src="${location}/${icu_url##*/}"
+
+    # Download, then use different extraction tools on Windows vs. otherwise
+    mkdir -p "${location}"
+    wget -nv -O "${src}" "${icu_url}"
+    if [ "${TRAVIS_OS_NAME?}" = windows ]
+    then
+        7z x "${src}" -o"${location}"
+    else
+        tar -xzf "${src}" -C "${location}"
+    fi
+}
+
+# Grab Python from the web then install on OSX
+# Handles using a cached version if available
+# Parameters:
+#  version: Python version to install
+function install_python_osx {
+    echo "Installing Python version ${1?} using pyenv..."
+    wget https://github.com/praekeltfoundation/travis-pyenv/releases/download/0.4.0/setup-pyenv.sh
+    # shellcheck disable=SC1091
+    PYENV_VERSION="${1}" PYENV_VERSION_STRING="Python ${1}" source setup-pyenv.sh
+}
+
+#############
+# Environment
+
+# Installation and build locations. Not the same so that we don't
+# make the cache huge by storing source and build artifacts.
+install_dir="$HOME/icu4c"
+build_dir="$HOME/build"
+
+# This will be used to uniquely determine if the correct cache is found or not.
+cache_tag="${install_dir}/icu4c-${TRAVIS_OS_NAME?}-${ICU_VERSION}.done"
+
+# The platform is used by runConfigureICU to determine what flags to set.
+case "${TRAVIS_OS_NAME}" in
+    osx) platform=MacOSX;;
+    windows) platform=Cygwin/MSVC ;;
+    *) platform=Linux ;;
+esac
+
+# Map Travis python version to a full major.minor.patch version
+if [ "${platform}" = Linux ]
+then
+    pyver="${TRAVIS_PYTHON_VERSION}"
+else
+    case "${TRAVIS_PYTHON_VERSION}" in
+        2.7) pyver=2.7.17;;
+        3.5) pyver=3.5.8;;
+        3.6) pyver=3.6.9;;
+        3.7) pyver=3.7.4;;
+        *)
+            if [ "${TRAVIS_PYTHON_VERSION}" != "3.8" ]
+            then
+                echo "Python version ${TRAVIS_PYTHON_VERSION} unset/unknown. Using default."
+            fi
+            pyver=3.8.0
+            ;;
+    esac
+fi
+
+echo "ICU environment information..."
+echo "INSTALLATION DIRECTORY: ${install_dir}"
+echo "BUILD DIRECTORY: ${build_dir}"
+echo "PYTHON VERSION: ${pyver}"
+echo "PLATFORM: ${platform}"
+echo "ICU VERSION: ${ICU_VERSION}"
+
+#######
+# Logic
+
+# Need to install Python on macos and windows (annoyingly).
+case "${TRAVIS_OS_NAME}" in
+    osx)
+        install_python_osx "${pyver}";;
+    windows)
+        choco install python --version "${pyver}";;
+esac
+
+# If the install_dir already exists, then it was cached and we do not need to rebuild.
+if [ -f "${cache_tag}" ]
+then
+    echo "Found cached icu4c in ${install_dir}, skipping build!"
+else
+    echo "Build and install ICU from source..."
+    fetch_icu "${ICU_VERSION}" "${build_dir}"
+
+    if [ "${TRAVIS_OS_NAME}" = windows ]
+    then
+        # XXX: As-is, this seems to be broken and does not work.
+        # It is unknown to me how to solve this at this time.
+        # Good luck future maintainer.
+        cd "${build_dir}/icu"
+        PATH="/c/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH"
+        MSBuild.exe source/allinone/allinone.sln -p:Configuration=Release -p:Platform=x64 -p:SkipUWP=true
+        mkdir "${install_dir}"
+        mv bin64 "${install_dir}/bin"
+        mv lib64 "${install_dir}/lib"
+        mv include "${install_dir}/include"
+    else
+        cd "${build_dir}/icu/source"
+        ./runConfigureICU "${platform}" --prefix="${install_dir}"
+        make
+        make install
+    fi
+
+    # Add a tag to the installation directory to validate the cache
+    touch "${cache_tag}"
+fi
+
+##############
+# Confirmation
+
+echo "Displaying ICU build information as confirmation..."
+export PATH="$HOME/icu4c/bin:${PATH}"
+export LD_LIBRARY_PATH="$HOME/icu4c/lib"
+export DYLD_LIBRARY_PATH="$HOME/icu4c/lib"
+echo "which icuinfo: $(command -v icuinfo)"
+echo "which icu-config: $(command -v icu-config)"
+echo "icuinfo -v:"
+icuinfo -v
+echo "icu-config --version: $(icu-config --version)"
+echo "icu-config --cxxflags: $(icu-config --cxxflags)"
+echo "icu-config --cppflags: $(icu-config --cppflags)"
