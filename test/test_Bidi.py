7ecb3ec8123cc1ddaca79182e3e2399ca6af0046
ovalhub <11970237+ovalhub@users.noreply.github.com>
2020-08-01 14:38:00 -0700

    fix broken test for ICU < 58

--- test/test_Bidi.py
+++ test/test_Bidi.py
@@ -76,16 +76,17 @@ class TestBidi(TestCase):
 
     def testTransform(self):
 
-        Locale.setDefault(Locale.getUS())
-        transform = BidiTransform()
+        if ICU_VERSION >= '58.0':
+            Locale.setDefault(Locale.getUS())
+            transform = BidiTransform()
 
-        result = transform.transform(
-            self.input_text[0],
-            UBiDiDirection.LTR, UBiDiOrder.VISUAL,
-            UBiDiDirection.RTL, UBiDiOrder.LOGICAL,
-            UBiDiMirroring.OFF, Shape.DIGITS_EN2AN)
+            result = transform.transform(
+                self.input_text[0],
+                UBiDiDirection.LTR, UBiDiOrder.VISUAL,
+                UBiDiDirection.RTL, UBiDiOrder.LOGICAL,
+                UBiDiMirroring.OFF, Shape.DIGITS_EN2AN)
 
-        self.assertEqual(result, UnicodeString(b'.\xd9\xa4K \xd8\xa9\xd9\x8a\xd9\x86\xd9\x82\xd8\xaa\xd8\xa8 \xd9\x84\xd9\x8a\xd8\xba\xd8\xb4\xd8\xaa \xd9\x85\xd8\xa7\xd8\xb8\xd9\x86 \xd9\x84\xd9\x88\xd8\xa3 \xd9\xa1\xd9\xa0 Windows \xd9\x86\xd8\xa7\xd9\x83', 'utf-8'))
+            self.assertEqual(result, UnicodeString(b'.\xd9\xa4K \xd8\xa9\xd9\x8a\xd9\x86\xd9\x82\xd8\xaa\xd8\xa8 \xd9\x84\xd9\x8a\xd8\xba\xd8\xb4\xd8\xaa \xd9\x85\xd8\xa7\xd8\xb8\xd9\x86 \xd9\x84\xd9\x88\xd8\xa3 \xd9\xa1\xd9\xa0 Windows \xd9\x86\xd8\xa7\xd9\x83', 'utf-8'))
 
 if __name__ == "__main__":
     main()
